4347720b2c6e7fca71d1eb83598706b4aed8feee
darth-vader-lg <luigi.generale@gmail.com>
2020-12-12 02:00:33 +0100

    Pulizia file di progetto dell'applicativo.

--- MachineLearningStudio/Program.cs
+++ MachineLearningStudio/Program.cs
@@ -15,6 +15,7 @@ namespace MachineLearningStudio
       [STAThread]
       private static void Main()
       {
+         Application.SetHighDpiMode(HighDpiMode.SystemAware);
          Application.EnableVisualStyles();
          Application.SetCompatibleTextRenderingDefault(false);
          Application.Run(new MainForm() { Text = Application.ProductName } );
