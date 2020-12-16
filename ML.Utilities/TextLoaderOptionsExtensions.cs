1375600467e5ca32b5b3f7650aa4bcdad423ac75
darth-vader-lg <luigi.generale@gmail.com>
2020-12-16 21:23:23 +0100

    Aggiunta classe di estensione alla TextLoader.Options con funzione di split di una riga di dati testuale.

---
+++ ML.Utilities/TextLoaderOptionsExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ML.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ML.Utilities
+{
+   /// <summary>
+   /// Estensioni alla classe TextLoader.Options
+   /// </summary>
+   public static class TextLoaderOptionsExtensions
+   {
+      #region Methods
+      /// <summary>
+      /// Splitta i dati di una riga in base alle colonne
+      /// </summary>
+      /// <param name="row">Riga di dati</param>
+      /// <returns>I dati splittati</returns>
+      public static string[] SplitData(this TextLoader.Options options, string row)
+      {
+         var expr = @"(?<match>\w+)|\""(?<match>[\w\s";
+         foreach (var separator in options.Separators != default && options.Separators.Length > 0 ? options.Separators : new[] { ',' })
+            expr += new string(separator, 1);
+         expr += @"]*)""";
+         return Regex.Matches(row, expr).Cast<Match>().Select(m => m.Groups["match"].Value).ToArray();
+      }
+      #endregion
+   }
+}
