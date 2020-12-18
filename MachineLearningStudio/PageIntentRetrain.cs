9fd390370f36e75b293dc0bc714026c9fb3ade3c
darth-vader-lg <luigi.generale@gmail.com>
2020-12-18 15:13:42 +0100

    Utilizzo della nuova classe textMeaningPredictor per la pagina di previsione intenzioni.

--- MachineLearningStudio/PageIntentRetrain.cs
+++ MachineLearningStudio/PageIntentRetrain.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.ML;
-using Microsoft.ML.Data;
 using Microsoft.ML.Runtime;
 using ML.Utilities;
+using ML.Utilities.Data;
+using ML.Utilities.Models;
+using ML.Utilities.Predictors;
 using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,33 +25,21 @@ namespace MachineLearningStudio
       /// </summary>
       private string dataSetName;
       /// <summary>
-      /// Sicronismo per l'aggiornamento dei dati
-      /// </summary>
-      private readonly object dataSetUpdateLocker = new object();
-      /// <summary>
       /// Flag di controllo inizializzato
       /// </summary>
       private bool initialized;
-      /// <summary>
-      /// Contesto ML
-      /// </summary>
-      private MLContext ml;
-      /// <summary>
-      /// Modello di apprendimento
-      /// </summary>
-      private TaskCompletionSource<ITransformer[]> model = new TaskCompletionSource<ITransformer[]>();
       /// <summary>
       /// Task di previsione
       /// </summary>
       private (Task task, CancellationTokenSource cancellation) taskPrediction = (Task.CompletedTask, new CancellationTokenSource());
       /// <summary>
-      /// Task di training continuo
-      /// </summary>
-      private (Task task, CancellationTokenSource cancellation) taskTrain = (Task.CompletedTask, new CancellationTokenSource());
-      /// <summary>
       /// Colore di background dei testi
       /// </summary>
       private readonly Color textBoxBackColor;
+      /// <summary>
+      /// Previsore di significato testi
+      /// </summary>
+      private TextMeaningPredictor textMeaningPredictor;
       #endregion
       #region Methods
       /// <summary>
@@ -104,7 +93,7 @@ namespace MachineLearningStudio
       private void Ml_Log(object sender, LoggingEventArgs e)
       {
          try {
-            if (e.Kind < ChannelMessageKind.Info || (e.Source != nameof(TaskTrain) && e.Source != nameof(TaskPrediction)))
+            if (e.Kind < ChannelMessageKind.Info)
                return;
             textBoxOutput.Invoke(new Action<LoggingEventArgs>(log =>
             {
@@ -130,9 +119,22 @@ namespace MachineLearningStudio
          // Metodo base
          try {
             base.OnLoad(e);
+            // Imposta il nome del file di dati
             textBoxDataSetName.Text = Settings.Default.PageIntent.DataSetName?.Trim();
-            ml = new MLContext();
-            ml.Log += Ml_Log;
+            // Crea il previsore
+            textMeaningPredictor = new TextMeaningPredictor
+            {
+               AutoCommitData = true,
+               AutoSaveModel = true,
+               DataStorage = new DataStorageTextFile(
+                  Path.Combine(Environment.CurrentDirectory, "Data", textBoxDataSetName.Text),
+                  columns: default,
+                  separator: '|'),
+               ModelStorage = new ModelStorageFile(Path.Combine(Environment.CurrentDirectory, "Data", Path.ChangeExtension(textBoxDataSetName.Text, "model.zip"))),
+            };
+            // Aggancia il log
+            textMeaningPredictor.ML.NET.Log += Ml_Log;
+            // Indicatore di inizializzazione ok
             initialized = true;
          }
          catch (Exception exc) {
@@ -151,6 +153,7 @@ namespace MachineLearningStudio
       private async Task TaskPrediction(string dataSetName, string sentence, CancellationToken cancel, TimeSpan delay = default, bool forceRebuildModel = false)
       {
          try {
+            // Attende la pausa
             await Task.Delay(delay, cancel);
             // Pulizia combo in caso di ricostruzione modello
             cancel.ThrowIfCancellationRequested();
@@ -158,146 +161,22 @@ namespace MachineLearningStudio
                textBoxOutput.Clear();
             cancel.ThrowIfCancellationRequested();
             // Rilancia o avvia il task di training
-            if (forceRebuildModel || taskTrain.task.IsCompleted) {
-               // Annulla quello attuale
-               taskTrain.cancellation.Cancel();
-               await taskTrain.task;
-               cancel.ThrowIfCancellationRequested();
-               // Lo rilancia
-               model = new TaskCompletionSource<ITransformer[]>();
-               taskTrain.cancellation = new CancellationTokenSource();
-               taskTrain.task = TaskTrain(dataSetName, taskTrain.cancellation.Token);
+            if (forceRebuildModel) {
+               await textMeaningPredictor.StopTrainingAsync(cancel);
+               await textMeaningPredictor.StartTrainingAsync(cancel);
             }
-            // Verifica se esiste un gestore di previsioni
             cancel.ThrowIfCancellationRequested();
-            // Attende il modello
-            var predicionModel = await model.Task;
-            // Effettua la previsione
-            if (predicionModel[0] != default && !string.IsNullOrWhiteSpace(sentence)) {
-               var predictionDataIn = ml.Data.LoadFromEnumerable(new[] { new { Label = "?", Sentence = sentence } });
-               var predictionDataOut = predicionModel[0].Transform(predictionDataIn);
-               textBoxIntent.Text = predictionDataOut.GetString("PredictedLabel");
-               textBoxIntent.BackColor = textBoxBackColor;
-            }
-            else {
-               textBoxIntent.Text = "";
-               textBoxIntent.BackColor = Color.Red;
-            }
+            textBoxIntent.Text = string.IsNullOrWhiteSpace(sentence) ? "" : await Task.Run(() => textMeaningPredictor.PredictAsync(new[] { null, sentence }));
+            textBoxIntent.BackColor = textBoxBackColor;
          }
          catch (OperationCanceledException) { }
          catch (Exception exc) {
             Trace.WriteLine(exc);
             textBoxIntent.Text = "";
             textBoxIntent.BackColor = Color.Red;
-            ml.WriteLog(exc.ToString(), nameof(TaskPrediction));
+            textMeaningPredictor.ML.NET.WriteLog(exc.ToString(), nameof(TaskPrediction));
          }
       }
-      /// <summary>
-      /// Task di training continuo
-      /// </summary>
-      /// <param name="dataSetName">Nome del set di dati</param>
-      /// <param name="cancel">Token di cancellazione</param>
-      /// <returns>Il task</returns>
-      private Task TaskTrain(string dataSetName, CancellationToken cancel) => Task.Factory.StartNew(async () =>
-      {
-         try {
-            // Path del set di dati in chiaro
-            var dataSetPath = !string.IsNullOrWhiteSpace(dataSetName) ? Path.Combine(Environment.CurrentDirectory, "Data", dataSetName) : null;
-            // Path del modello
-            var modelPath = !string.IsNullOrWhiteSpace(dataSetPath) ? Path.ChangeExtension(dataSetPath, "model.zip") : null;
-            // Carica modello esistente
-            if (modelPath != default && File.Exists(modelPath)) {
-               var m = ml.Model.Load(modelPath, out _);
-               if (!model.TrySetResult(new[] { m }))
-                  model.Task.Result[0] = m;
-            }
-            if (dataSetPath != default && File.Exists(dataSetPath)) {
-               // Trainer
-               var trainer = ml.MulticlassClassification.Trainers.SdcaNonCalibrated();
-               // Pipe di trasformazione
-               var pipe =
-                  ml.Transforms.Conversion.MapValueToKey("Label").
-                  Append(ml.Transforms.Text.FeaturizeText("Sentence_tf", "Sentence")).
-                  Append(ml.Transforms.CopyColumns("Features", "Sentence_tf")).
-                  Append(ml.Transforms.NormalizeMinMax("Features")).
-                  AppendCacheCheckpoint(ml).
-                  Append(trainer).
-                  Append(ml.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
-               // Task di salvataggio del modello
-               var (taskSaveModel, cancSaveModel) = (Task.CompletedTask, CancellationTokenSource.CreateLinkedTokenSource(cancel));
-               // Loop di training continuo
-               var prevMetrics = default(MulticlassClassificationMetrics);
-               var prevDataSetTime = File.GetLastWriteTime(dataSetPath) - new TimeSpan(1, 0, 0);
-               var seed = 0;
-               while (!cancel.IsCancellationRequested) {
-                  try {
-                     // Time stamp del file di dati
-                     var dataSetTime = File.GetLastWriteTime(dataSetPath);
-                     // Copia il file temporaneo se il file di dati e' stato aggiornato
-                     if (dataSetTime > prevDataSetTime) {
-                        lock (dataSetUpdateLocker)
-                           File.Copy(dataSetPath, $"{dataSetPath}.trn", true);
-                     }
-                     // Dati di input
-                     var dataView = ml.Data.LoadFromTextFile(
-                        path: $"{dataSetPath}.trn",
-                        columns: new[]
-                        {
-                           new TextLoader.Column("Label", DataKind.String, 0),
-                           new TextLoader.Column("Sentence", DataKind.String, 1),
-                        },
-                        hasHeader: false,
-                        separatorChar: '|',
-                        allowQuoting: true,
-                        allowSparse: false);
-                     // Effettua il training
-                     var model = pipe.Fit(ml.Data.ShuffleRows(dataView, seed++));
-                     var metrics = ml.MulticlassClassification.Evaluate(model.Transform(dataView));
-                     //var crossValidation = ml.MulticlassClassification.CrossValidate(dataView, pipe, 5, "Label", null, seed++);
-                     //var best = crossValidation.Best();
-                     //var model = best.Model;
-                     //var metrics = best.Metrics;
-                     cancel.ThrowIfCancellationRequested();
-                     // Verifica se c'e' un miglioramento; se affermativo salva il nuovo modello
-                     if (prevMetrics == default || dataSetTime > prevDataSetTime || (metrics.MicroAccuracy >= prevMetrics.MicroAccuracy && metrics.LogLoss < prevMetrics.LogLoss)) {
-                        // Emette il log
-                        ml.WriteLog("Found best model", nameof(TaskTrain));
-                        ml.WriteLog(metrics.ToText(), nameof(TaskTrain));
-                        cancel.ThrowIfCancellationRequested();
-                        // Annulla eventuali task di salvataggio precedenti
-                        cancSaveModel.Cancel();
-                        await taskSaveModel;
-                        cancel.ThrowIfCancellationRequested();
-                        // Avvia il task di salvataggio
-                        cancSaveModel = CancellationTokenSource.CreateLinkedTokenSource(cancel);
-                        var savingModel = model;
-                        taskSaveModel = Task.Run(() =>
-                        {
-                           cancSaveModel.Token.ThrowIfCancellationRequested();
-                           ml.Model.Save(savingModel, dataView.Schema, modelPath);
-                        }, cancSaveModel.Token);
-                        prevMetrics = metrics;
-                        prevDataSetTime = dataSetTime;
-                        // Aggiorna il modello attuale
-                        if (!this.model.TrySetResult(new[] { model }))
-                           this.model.Task.Result[0] = model;
-                     }
-                  }
-                  catch (OperationCanceledException) { }
-                  catch (Exception exc) {
-                     Trace.WriteLine(exc);
-                     ml.WriteLog(exc.Message, nameof(TaskTrain));
-                  }
-               }
-            }
-         }
-         catch (Exception exc) {
-            Trace.WriteLine(exc);
-            ml.WriteLog(exc.ToString(), nameof(TaskTrain));
-         }
-         if (!model.TrySetResult(new ITransformer[] { default }))
-            model.Task.Result[0] = default;
-      }, cancel, TaskCreationOptions.LongRunning, TaskScheduler.Default);
       /// <summary>
       /// Evento di variazione del testo del nome del set di dati
       /// </summary>
@@ -352,38 +231,8 @@ namespace MachineLearningStudio
                return;
             if (string.IsNullOrWhiteSpace(dataSetName))
                return;
-            var savingDataPath = Path.Combine(Environment.CurrentDirectory, "Data", dataSetName);
-            var savingSentence = textBoxSentence.Text.Trim().ToLower();
-            var savingIntent = tb.Text;
-            Task.Run(() =>
-            {
-               try {
-                  lock (dataSetUpdateLocker) {
-                     using var reader = new StreamReader(savingDataPath);
-                     using var writer = new StreamWriter($"{savingDataPath}.new");
-                     var found = false;
-                     for (var line = reader.ReadLine(); line != null; line = reader.ReadLine()) {
-                        var items = line.Split('|', 2);
-                        if (items[1].ToLower() == savingSentence) {
-                           if (items[0] != savingIntent) {
-                              items[0] = savingIntent;
-                              found = true;
-                           }
-                        }
-                        writer.WriteLine($"{items[0]}|{items[1]}");
-                     }
-                     if (!found)
-                        writer.WriteLine($"{savingIntent}|{savingSentence}");
-                     reader.Close();
-                     writer.Close();
-                     File.Copy($"{savingDataPath}.new", savingDataPath, true);
-                     File.Delete($"{savingDataPath}.new");
-                  }
-               }
-               catch (Exception exc) {
-                  Trace.WriteLine(exc);
-               }
-            });
+            textMeaningPredictor.AppendData(textBoxIntent.Text.Trim(), textBoxSentence.Text.Trim());
+            MakePrediction(default, true);
          }
          catch (Exception exc) {
             Trace.WriteLine(exc);
