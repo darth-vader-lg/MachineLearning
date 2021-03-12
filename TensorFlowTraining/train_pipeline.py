e4472f7a2e7f9deacb9be07473b6426e8814b2fd
darth-vader-lg <luigi.generale@gmail.com>
2021-03-12 01:41:35 +0100

    Updated automatically the number of classes in the pipeline.config generated file.

--- TensorFlowTraining/train_pipeline.py
+++ TensorFlowTraining/train_pipeline.py
@@ -15,17 +15,21 @@ def config_train_pipeline(prm: TrainParameters):
     """
     import tensorflow as tf
     from object_detection.protos import pipeline_pb2
+    from object_detection.utils import label_map_util
     from google.protobuf import text_format
     # Copy the pipeline configuration file if it's not already present in the output dir
     print('Configuring the pipeline')
     output_file = prm.pipeline_config_path
     shutil.copy2(os.path.join(prm.pre_trained_model_dir, 'pipeline.config'), output_file)
+    # Read the number of labels
+    label_dict = label_map_util.get_label_map_dict(os.path.join(prm.annotations_dir, 'label_map.pbtxt'))
+    labels_count = len(label_dict)
     # Configuring the pipeline
     pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
     with tf.io.gfile.GFile(output_file, "r") as f:
         proto_str = f.read()
         text_format.Merge(proto_str, pipeline_config)
-    pipeline_config.model.ssd.num_classes = 1 # TODO define
+    pipeline_config.model.ssd.num_classes = labels_count
     pipeline_config.model.ssd.image_resizer.fixed_shape_resizer.height = prm.model['height']
     pipeline_config.model.ssd.image_resizer.fixed_shape_resizer.width = prm.model['width']
     pipeline_config.train_config.batch_size = prm.model['batch_size']
@@ -43,5 +47,5 @@ def config_train_pipeline(prm: TrainParameters):
     print(str(config_text))
 
 if __name__ == '__main__':
-    prm = prm or TrainParameters.default
+    prm = ('prm' in locals() and prm) or TrainParameters.default
     config_train_pipeline(TrainParameters.default)
