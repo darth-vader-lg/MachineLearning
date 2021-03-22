5bc76a5c6f06dafda6b7da8819c8435290850261
darth-vader-lg <luigi.generale@gmail.com>
2021-03-22 02:04:00 +0100

    Modello di rilevamento oggetti ereditato dal modello base TensorFlow.

---
+++ MachineLearning/Model/ModelBaseTensorFlow.cs
@@ -0,0 +1,26 @@
+﻿using MachineLearning.TensorFlow;
+using System;
+
+namespace MachineLearning.Model
+{
+   /// <summary>
+   /// Classe base per i modelli TensorFlow
+   /// </summary>
+   [Serializable]
+   public abstract partial class ModelBaseTensorFlow : ModelBase, IContextProvider<TFContext>
+   {
+      #region Properties
+      /// <summary>
+      /// Contesto
+      /// </summary>
+      public TFContext Context => ((IContextProvider<TFContext>)GetChannelProvider()).Context; 
+      #endregion
+      #region Methods
+      /// <summary>
+      /// Costruttore
+      /// </summary>
+      /// <param name="contextProvider">Provider di contesto di machine learning</param>
+      public ModelBaseTensorFlow(IContextProvider<TFContext> contextProvider = default) : base(contextProvider ?? MachineLearningContext.Default) { }
+      #endregion
+   }
+}
--- MachineLearning/ObjectDetection.cs
+++ MachineLearning/ObjectDetection.cs
@@ -75,13 +75,13 @@ namespace MachineLearning
    public sealed partial class ObjectDetection // Prediction
    {
       [Serializable]
-      public sealed class Model
+      public sealed class Model : ModelBaseTensorFlow, IDataTransformer
       {
          #region Fields
          /// <summary>
-         /// Contesto di machine learning
+         /// Label degli oggetti conosciuti dal modello
          /// </summary>
-         private readonly IContextProvider<TFContext> _context;
+         private PbtxtItems _labels;
          /// <summary>
          /// Oggetto di appartenenza
          /// </summary>
@@ -93,18 +93,30 @@ namespace MachineLearning
          /// <summary>
          /// Soglia punteggio minimo
          /// </summary>
-         public float MIN_SCORE = 0.5f;
+         private const float MIN_SCORE = 0.5f;
          #endregion
+         /// <summary>
+         /// Schema di output
+         /// </summary>
+         private DataViewSchema OutputSchema { get; }
          #region Methods
          /// <summary>
          /// Costruttore
          /// </summary>
          /// <param name="owner">Oggetto di appartenenza</param>
-         /// <param name="context">Contesto di machine learning</param>
-         internal Model(ObjectDetection owner, IContextProvider<TFContext> context)
+         /// <param name="contextProvider">Provider di contesto di machine learning</param>
+         internal Model(ObjectDetection owner, IContextProvider<TFContext> contextProvider = default) : base(contextProvider)
          {
             _owner = owner;
-            _context = context;
+            OutputSchema = DataViewSchemaBuilder.Build(
+               ("ImagePath", typeof(string)),
+               ("ObjectId", typeof(int)),
+               ("ObjectName", typeof(string)),
+               ("ObjectDisplayName", typeof(string)),
+               ("ObjectLeft", typeof(float)),
+               ("ObjectTop", typeof(float)),
+               ("ObjectWidth", typeof(float)),
+               ("ObjectHeight", typeof(float)));
          }
          /// <summary>
          /// Crea l'immagine di output
@@ -113,8 +125,6 @@ namespace MachineLearning
          /// <param name="resultArr">Risultato della previsione</param>
          private void BuildOutputImage(string inputImagePath, NDArray[] resultArr)
          {
-            // Elenco di oggetti del modello
-            var pbTxtItems = PbtxtParser.ParsePbtxtFile(Path.Combine(_owner.ModelStorage, "label_map.pbtxt"));
             // Crea il bitmap dall'immagine
             var bitmap = new Bitmap(inputImagePath);
             var scores = resultArr[2].AsIterator<float>();
@@ -134,7 +144,7 @@ namespace MachineLearning
                      Width = (int)(right - left),
                      Height = (int)(bottom - top)
                   };
-                  var name = pbTxtItems.Items.Where(w => w.Id == id[i])
+                  var name = _labels.Items.Where(w => w.Id == id[i])
                      .Select(s => !string.IsNullOrWhiteSpace(s.DisplayName) ? s.DisplayName : s.Name)
                      .FirstOrDefault();
                   DrawObjectOnBitmap(bitmap, rect, score, name);
@@ -169,13 +179,16 @@ namespace MachineLearning
          /// <param name="data">Riga di dati da usare per la previsione</param>
          /// <param name="cancellation">Eventule token di cancellazione attesa</param>
          /// <returns>La previsione</returns>
-         public async Task<IDataAccess> GetPredictionDataAsync(IEnumerable<object> data, CancellationToken cancellation)
+         public new async Task<IDataAccess> GetPredictionDataAsync(IEnumerable<object> data, CancellationToken cancellation)
          {
+            //return await base.GetPredictionDataAsync(data, cancellation);
             // Importa il grafico del modello
             await Task.Run(() =>
             {
-               if (_session == null)
+               if (_session == null) {
                   _session = Session.LoadFromSavedModel(Path.Combine(_owner.ModelStorage, "saved_model"));
+                  _labels = PbtxtParser.ParsePbtxtFile(Path.Combine(_owner.ModelStorage, "label_map.pbtxt"));
+               }
                foreach (string img in data) {
                   var imgArr = ReadTensorFromImageFile(img);
                   var graph = _session.as_default().graph.as_default();
@@ -193,23 +206,115 @@ namespace MachineLearning
             return null;
          }
          /// <summary>
+         /// Trasforma i dati di input per il modello
+         /// </summary>
+         /// <param name="data">Dati di input</param>
+         /// <param name="cancellation">Eventuale token di cancellazione</param>
+         /// <returns>I dati trasformati</returns>
+         IDataAccess IDataTransformer.Transform(IDataAccess data, CancellationToken cancellation)
+         {
+            // Crea la sessione di trasformazione
+            cancellation.ThrowIfCancellationRequested();
+            // Prepara la configurazione della rete neurale
+            var graph = _session.as_default().graph.as_default();
+            var t0 = graph.OperationByName("StatefulPartitionedCall").outputs;
+            var tensorNum = t0[5];
+            var tensorBoxes = t0[1];
+            var tensorScores = t0[4];
+            var tensorClasses = t0[2];
+            var imgTensor = graph.OperationByName("serving_default_input_tensor").outputs[0];
+            var outTensorArr = new Tensor[] { tensorNum, tensorBoxes, tensorScores, tensorClasses };
+            // Dati di input e di output
+            var input = data.ToDataViewGrid();
+            var output = DataViewGrid.Create(this, OutputSchema);
+            // Loop su tutte le righe di input
+            foreach (var row in data.GetRowCursor(data.Schema).AsEnumerable()) {
+               // Path dell'immagine
+               var imagePath = row.ToDataViewValuesRow(this)["ImagePath"];
+               // Trasforma l'immagine in dati numerici
+               var imgArr = ReadTensorFromImageFile(imagePath);
+               // La passa per la rete neurale
+               cancellation.ThrowIfCancellationRequested();
+               var results = _session.as_default().run(outTensorArr, new FeedItem(imgTensor, imgArr));
+               // Ottiene i risultati
+               cancellation.ThrowIfCancellationRequested();
+               var scores = results[2].AsIterator<float>();
+               var boxes = results[1].GetData<float>();
+               var ids = np.squeeze(results[3]).GetData<float>();
+               // Riempe la vista di dati di output con le rilevazioni
+               for (var i = 0; i < scores.size; i++) {
+                  var score = scores.MoveNext();
+                  if (score < MIN_SCORE)
+                     continue;
+                  var label = _labels.Items.Where(w => w.Id == ids[i]).FirstOrDefault();
+                  if (label == default)
+                     continue;
+                  output.Add(
+                     ("ImagePath", imagePath),
+                     ("ObjectId", label.Id),
+                     ("ObjectName", label.Name),
+                     ("ObjectDisplayName", label.DisplayName),
+                     ("ObjectLeft", boxes[i * 4 + 1]),
+                     ("ObjectTop", boxes[i * 4]),
+                     ("ObjectWidth", boxes[i * 4 + 3] - boxes[i * 4 + 1]),
+                     ("ObjectHeight", boxes[i * 4 + 2] - boxes[i * 4]));
+               }
+            }
+            return output;
+         }
+         /// <summary>
+         /// Carica i dati da uno storage
+         /// </summary>
+         /// <param name="dataStorage">Storage di dati</param>
+         /// <returns>La vista di dati</returns>
+         public override IDataAccess LoadData(IDataStorage dataStorage) => null;
+         /// <summary>
+         /// Carica il modello da uno storage
+         /// </summary>
+         /// <param name="modelStorage">Storage del modello</param>
+         /// <param name="schema">Lo schema del modello</param>
+         /// <returns>Il modello</returns>
+         public override IDataTransformer LoadModel(IModelStorage modelStorage, out DataViewSchema schema)
+         {
+            // Disalloca l'eventuale modello precedente
+            if (_session != null)
+               _session.Dispose();
+            // Carica modello e labels
+            _session = Session.LoadFromSavedModel(Path.Combine(_owner.ModelStorage, "saved_model"));
+            _labels = PbtxtParser.ParsePbtxtFile(Path.Combine(_owner.ModelStorage, "label_map.pbtxt"));
+            schema = OutputSchema;
+            return this;
+         }
+         /// <summary>
          /// Legge un immagine e ne crea il tensore
          /// </summary>
          /// <param name="filePath">Path del file</param>
          /// <returns>Il tensore</returns>
          private NDArray ReadTensorFromImageFile(string filePath)
          {
-            var tf = _context.Context;
-            using var graph = tf.Graph().as_default();
-            tf.compat.v1.disable_eager_execution();
-            using var file_reader = tf.io.read_file(filePath, "file_reader");
-            using var decodeJpeg = tf.image.decode_jpeg(file_reader, channels: 3, name: "DecodeJpeg");
-            using var casted = tf.cast(decodeJpeg, TF_DataType.TF_UINT8);
-            using var dims_expander = tf.expand_dims(casted, 0);
-            tf.enable_eager_execution();
-            using var sess = tf.Session(graph).as_default();
+            using var graph = Context.Graph().as_default();
+            Context.compat.v1.disable_eager_execution();
+            using var file_reader = Context.io.read_file(filePath, "file_reader");
+            using var decodeJpeg = Context.image.decode_jpeg(file_reader, channels: 3, name: "DecodeJpeg");
+            using var casted = Context.cast(decodeJpeg, TF_DataType.TF_UINT8);
+            using var dims_expander = Context.expand_dims(casted, 0);
+            Context.enable_eager_execution();
+            using var sess = Context.Session(graph).as_default();
             return sess.run(dims_expander);
          }
+         /// <summary>
+         /// Salva i dati in uno storage
+         /// </summary>
+         /// <param name="dataStorage">Storage di dati</param>
+         /// <param name="data">Dati</param>
+         public override void SaveData(IDataStorage dataStorage, IDataAccess data) { }
+         /// <summary>
+         /// Salva il modello in uno storage
+         /// </summary>
+         /// <param name="modelStorage">Storage del modello</param>
+         /// <param name="model">Modello</param>
+         /// <param name="schema">Lo schema del modello</param>
+         public override void SaveModel(IModelStorage modelStorage, IDataTransformer model, DataViewSchema schema) { }
          #endregion
       }
    }
