52f629bdb105d6102853d3af0ed972c45499ab51
darth-vader-lg <luigi.generale@gmail.com>
2021-03-30 13:52:21 +0200

    - Prepend of the paths of the environment before the installation of the requirements. - Added the --upgrade flag for the installation of the requirements.

--- TensorFlowTraining/InstallVirtualEnvironment.bat
+++ TensorFlowTraining/InstallVirtualEnvironment.bat
@@ -4,14 +4,15 @@ if not exist env (
    py -m venv env
 )
 if ERRORLEVEL 1 EXIT /B %errorlevel%
-call env\Scripts\activate.bat
+set PATH=%~dp0\env\Scripts;%~dp0;%PATH%
+echo %PATH%
 if not exist env\Lib\site-packages\PyInstaller (
    echo Installing the dependencies
-   py -m pip install -r requirements.txt
+   python -m pip install --upgrade -r requirements.txt
 )
 if ERRORLEVEL 1 EXIT /B %errorlevel%
 if not exist env\Lib\site-packages\object_detection (
    echo Installing the object detection API
-   py od_install.py
+   python od_install.py
 )
 EXIT /B %errorlevel%
